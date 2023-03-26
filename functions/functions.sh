dca44320545c6f74a18850a50a8b29caa59e770f
taquero-programador <jrangel93@protonmail.com>
2023-03-25 21:33:40 -0600

    feat(functions/functions.sh): add file to append functions

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# tmux function to create session
+# constructor
 tmx_call() {
     tmux new -d -s debian -n vim
     tmux new-window -n "git&comamnds" \; split-window -v
@@ -9,12 +9,30 @@ tmx_call() {
     tmux a -t debian:ncmpcpp
 }
 
-# function eval to create tmux session
 tmx() {
-    tmux ls &> /dev/null
-    if [[ $? -ne 0 ]]; then
+    # save name of session if exists
+    session_name=$(tmux ls &> /dev/null | grep -io "debian")
+    # save if session is now attached
+    session_att=$(tmux ls &> /dev/null | grep -io "attached")
+    # var saved in $? (code exit)
+    test_tmx=$(tmux ls &> /dev/null) # is a same to tmux ls &> /dev/null without $()
+
+    # if $? is not equal to 0, create session
+    if [[ ! $? -eq 0 ]]; then
+        echo "Creating session..."
+        sleep 1
         tmx_call
-    elif tmux ls | grep -i "debian" &> /dev/null; then
-        tmux a -t debian
+    # $session_name is not empty
+    elif [[ -n $session_name ]]; then
+        # two vars compare
+        if [[ $session_name = "debian" && $session_att = "attached" ]]; then
+            # show info
+            echo -e "$(tmux ls)\nSession in use! Try: tmux a -t $session_name"
+        # session exists, but no attached
+        elif [[ $session_name = "debian" && -z $session_att ]]; then
+            echo "Attach to $session_name..."
+            sleep 1
+            tmux a -t $session_name
+        fi
     fi
 }
